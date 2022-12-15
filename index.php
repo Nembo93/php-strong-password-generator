88f45afd331cd1fc42a1a6345e7d03d88062d512
nembo93 <francesco.c193@libero.it>
2022-12-15 15:37:12 +0100

    Aggiunge form e salva in una variabile il valore (lunghezza della password da generare)

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+// Dichiarare una variabile per la lunghezza della password e assegnarli il valore del form tramite GET
+$password_len = "";
+$password_len = $_GET["password_number"];
+var_dump($password_len);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +16,9 @@
 </head>
 <body>
     <h1>Password</h1>
+    <form action="" method="get">
+        <input type="number" name="password_number">
+        <button>Genera</button>
+    </form>
 </body>
 </html>
