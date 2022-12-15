e687c0a277730650069c9db98d24e14c73bc2d48
nembo93 <francesco.c193@libero.it>
2022-12-15 17:51:54 +0100

    Modifica ciclo mcon condizione per creare password alphanumerica

--- index.php
+++ index.php
@@ -1,25 +1,37 @@
 <?php
 // Dichiarare degli array con i caratteri per generare la password
 // $pas_num_char = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-$pas_letter_char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "l"];
+// $pas_letter_char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "l"];
 // var_dump($pas_num_char, $pas_letter_char);
 // Dichiarare una variabile per la lunghezza della password e assegnarli il valore del form tramite GET
-// $password_len = "";
-// $password_len = $_GET["password_number"];
-// var_dump($password_len);
 
 // Creare una funzione generi una password di lunghezza pari al valore preso dal form
+    // 1. Dichiarare l'array da cui attingere i numeri per generare la password all'interno della funzione
+    // 2. Dichiarare in una variabile il valore del form (lunghezza della password)
+    // 3. Creare condizione che verificando la presenza di un valore accettato all'interno del form, dichiari un array in cui pushare la password generata ed entri nel ciclo
+    // 4. Creare ciclo che si esegua per un numero di volte pari alla lunghezza della password da generare
+        // 4.1 Creare, all'interno del ciclo, funzione che generi un numero randomico (indice dell'array da cui attingere i numeri della password)
+        // 4.2 Richiamare tramite indice (numero randomico) elemento dell'array da cui attingere numeri e pusharlo all'interno dell'array della password da generare
+
 function password_gen (){
-    $pas_num_char = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     $pass_len = $_GET["password_number"];
+    $pas_num_char = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    $pas_letter_char = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "l"];
 
     if ($_GET["password_number"] != 0 && $_GET["password_number"] != ""){
         echo "dato presente";
-        // Ciclo
         $my_password = [];
+        // Ciclo
         for ($i = 0; $i < $pass_len; $i++){
-            $num_rand = rand(0, (count($pas_num_char)) - 1);
-            $my_password[] = $pas_num_char[$num_rand];
+            $rand_char = rand(0, 9);
+            var_dump($rand_char);
+            if($rand_char % 2 == 0){
+                $num_rand = rand(0, (count($pas_num_char)) - 1);
+                $my_password[] = $pas_num_char[$num_rand];
+            } else {
+                $num_rand = rand(0, (count($pas_letter_char)) - 1);
+                $my_password[] = $pas_letter_char[$num_rand];
+            }
             }
         var_dump($my_password);
     } else{
@@ -55,3 +67,7 @@ password_gen();
     </form>
 </body>
 </html>
+
+
+<!-- $num_rand = rand(0, (count($pas_num_char)) - 1);
+$my_password[] = $pas_num_char[$num_rand]; -->
